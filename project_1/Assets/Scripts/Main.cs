d563f236e5d0592f10f0f3a0780fe1c0f435b1b4
seujuj <seujuj@gmail.com>
2023-01-24 14:37:20 +0900

    더미 환경

---
+++ project_1/Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         GameObject _field = GameObject.Find("BG");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveBG()
+    {
+        
+    }
+}
